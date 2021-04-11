27cbf5b964c57a7c04414fe2d9e4edb1e02e8d89
Ngireddy <somulanagireddy@gmail.com>
2021-04-11 19:07:40 +0530

    [Nagireddy]: added Address book structure

---
+++ src/main/java/Bridgelabzs/AddressBook.java
@@ -0,0 +1,66 @@
+package Bridgelabzs;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook {
+
+    String currentAddressBookName;
+    ArrayList<Contact> currentAddressBook = new ArrayList<Contact>();
+
+
+    public AddressBook(String currentAddressBookName) {
+        super();
+        this.currentAddressBookName = currentAddressBookName;
+    }
+
+
+    public void addContact (Scanner sc) {
+        System.out.println("Enter Details of Contact you want to add -");
+
+        System.out.println("Please Enter First Name");
+        String firstName = sc.next();
+        System.out.println("Please Enter Last Name");
+        String lastName = sc.next();
+        System.out.println("Please Enter Address");
+        String address = sc.next();
+        System.out.println("Please Enter City");
+        String city = sc.next();
+        System.out.println("Please Enter State");
+        String state = sc.next();
+        System.out.println("Please Enter Zip Code");
+        int zipCode = sc.nextInt();
+        System.out.println("Please Enter Phone Number");
+        long phoneNumber = sc.nextLong();
+        System.out.println("Please Enter Email Address");
+        String emailAddress = sc.next();
+
+
+        Contact currentContact = new Contact(firstName,lastName, address, city, state,
+                emailAddress, zipCode, phoneNumber);
+        this.currentAddressBook.add(currentContact);
+
+    }
+
+
+
+
+    public void displayBook(AddressBook passedAddressBook) {
+
+        System.out.printf("Address Book Name is %s. \n\n", passedAddressBook.currentAddressBookName);
+        Contact blankContact = new Contact(null, null, null, null, null, null, 0, 0);
+
+        System.out.printf("\n\n----------------------------------------------------\n\n");
+        for (int i = 0 ; i<passedAddressBook.currentAddressBook.size(); i++) {
+            System.out.printf("Contact %d :\n",i+1);
+
+            //How can use the following function w/o having to make a blank object to access it?
+            blankContact.displayContact(passedAddressBook.currentAddressBook.get(i));
+
+            System.out.printf("\n\n----------------------------------------------------\n\n");
+
+        }
+
+    }
+
+}
---
+++ src/main/java/Bridgelabzs/AddressBookMain.java
@@ -0,0 +1,22 @@
+package Bridgelabzs;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        System.out.printf("Welcome to AddressBook Program.\n\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        AddressBook book1 = new AddressBook("Book1");
+        book1.addContact(sc);
+        book1.addContact(sc);
+
+        book1.displayBook(book1);
+
+
+
+    }
+
+}
